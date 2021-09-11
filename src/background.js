b55f26f706d6ae1479fcdbd25b31e8583bed456f
Tom <tom@tomlemmens.be>
2021-09-11 15:36:34 +0200

    Adapt to use WhatsApp Web, unfortunately site doesn't work

--- src/background.js
+++ src/background.js
@@ -4,7 +4,7 @@ function debug_msg(message){
   if(is_release){
     return;
   }
-  console.debug("Google-Keep-Tab: " + message);
+  console.debug("Whatstab: " + message);
 }
 
 debug_msg("Loading");
@@ -18,10 +18,10 @@ if ("browserAction" in browser) {
   debug_msg("Has permision for browserAction");
 
   browser.browserAction.onClicked.addListener(async () => {
-    openNewTab("https://keep.google.com");
+    openNewTab("http://web.whatsapp.com");
   });
 } else {
-  openNewTab("https://keep.google.com");
+  openNewTab("http://web.whatsapp.com");
 }
 
 debug_msg("Loaded");
